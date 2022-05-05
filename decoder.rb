ce991162db4a998e191c5077e1b30576d30b4092
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-05 14:13:02 +0200

    Adding the decode logic in pair

---
+++ decoder.rb
@@ -0,0 +1,67 @@
+$morse_char = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0'
+}
+
+def decode_char(char)
+  $morse_char[char].upcase
+end
+
+def decode_word(word)
+  result = ''
+  word.split(' ').each do |n|
+    result += decode_char(n)
+  end
+  result
+end
+
+def decode(sentence)
+  result = ''
+  sentence.split('   ').each_with_index  do |n, i|
+    if i == 0
+      result += decode_word(n)
+      else
+        result += ' '
+        result += decode_word(n)
+    end
+  end
+  result
+end
+
+puts decode_char('.-')
+puts decode_word("-- -.--")
+p decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
